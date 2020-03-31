61cee67ad83e719624942e622a45a7d3b54a8bdb
okarinbro <prz.jab98@gmail.com>
2020-03-31 19:17:02 +0200

    Remove advanced string operations

--- Lab_2/Mparser.py
+++ Lab_2/Mparser.py
@@ -7,7 +7,9 @@ literals = scanner.literals
 
 precedence = (
     ("left", '*', '/'),
-    ("left", '+', '-')
+    ("left", 'DOTMUL', 'DOTDIV'),
+    ("left", '+', '-'),
+    ("left", 'DOTADD', 'DOTSUB')
 )
 
 
@@ -59,20 +61,19 @@ def p_instruction_2(p):
     p[0] = (p[1], p[2])
 
 
-def p_instruction_3(p):
-    """instruction : '{' instructions '}'"""
-    p[0] = (p[2])
-
-
-def p_printable_1(p):
-    """ printable : EXPRESSION
-                 | STRING"""
-    p[0] = (p[1])
-
-
 def p_printable_2(p):
     """ printable : printable ',' EXPRESSION"""
     p[0] = ('printable', p[1], p[3])
+
+
+def p_printable_1(p):
+    """printable : EXPRESSION"""
+    p[0] = p[1]
+
+
+def p_instruction_3(p):
+    """instruction : '{' instructions '}'"""
+    p[0] = (p[2])
 
 
 def p_loop_1(p):
@@ -88,6 +89,7 @@ def p_loop_2(p):
 def p_array_range(p):
     """ array_range : ID '=' INTNUM ':' ID
                     | ID '=' ID ':' ID
+                    | ID '=' ID ':' INTNUM
                     | ID '=' INTNUM ':' INTNUM"""
     p[0] = ('array_range', p[1], 'from', p[3], 'to', p[5])
 
@@ -126,18 +128,17 @@ def p_logical_operator(p):
 
 def p_assign_1(p):
     """assign : ID '=' EXPRESSION
-              | ID ADDASSIGN STRING
-              | ID DIVASSIGN EXPRESSION
-              | ID SUBASSIGN EXPRESSION
-              | ID MULASSIGN EXPRESSION
-              | ID ADDASSIGN EXPRESSION
+              | ID ADDASSIGN string_expression
+              | ID DIVASSIGN operable_expression
+              | ID SUBASSIGN operable_expression
+              | ID MULASSIGN operable_expression
+              | ID ADDASSIGN operable_expression
               """
     p[0] = ('assign', p[1], p[2], p[3])
 
 
 def p_assign_2(p):
-    """assign : ID '=' STRING '+' STRING
-              | ID '[' introw ']' '=' EXPRESSION"""
+    """assign : ID '[' introw ']' '=' EXPRESSION"""
     p[0] = ('assign', p[1:])
 
 
@@ -150,17 +151,33 @@ def p_introw_1(p):
         p[0] = p[1]
 
 
+def p_expression(p):
+    """EXPRESSION : operable_expression
+                  | string_expression """
+    p[0] = p[1]
+
+
+def p_string_expression(p):
+    """string_expression :  string_expression '+' string_expression """
+    p[0] = ('binop', p[1], p[2], p[3])
+
+
+def p_string_expression_1(p):
+    """string_expression : STRING"""
+    p[0] = ('var', p[1])
+
+
 def p_expression_1(p):
-    """EXPRESSION : EXPRESSION '+' EXPRESSION
-                  | EXPRESSION '-' EXPRESSION
-                  | EXPRESSION '*' EXPRESSION
-                  | EXPRESSION '/' EXPRESSION """
+    """operable_expression : operable_expression '+' operable_expression
+                  | operable_expression '-' operable_expression
+                  | operable_expression '*' operable_expression
+                  | operable_expression '/' operable_expression """
     p[0] = ('binop', p[1], p[2], p[3])
 
 
 def p_expression_4(p):
-    """EXPRESSION : ID '[' introw ']'
-                  | '(' EXPRESSION ')'"""
+    """operable_expression : ID '[' introw ']'
+                           | '(' operable_expression ')'"""
     if len(p) == 5:
         p[0] = ('array_part', p[1], p[3])
     else:
@@ -168,13 +185,13 @@ def p_expression_4(p):
 
 
 def p_expression_5(p):
-    """EXPRESSION : '-' EXPRESSION"""
+    """operable_expression : '-' operable_expression"""
     p[0] = ('negative', p[2])
 
 
 def p_expression_6(p):
-    """EXPRESSION : matrix_expression
-                  | numeric_expression"""
+    """operable_expression : matrix_expression
+                           | numeric_expression"""
     p[0] = p[1]
 
 
@@ -211,7 +228,7 @@ def p_m_expression_3(p):
 def p_create_matrix(p):
     """matrix_expression : matrix
                          | ID"""
-    p[0] = p[1]
+    p[0] = ('var', p[1])
 
 
 def p_matrix(p):
