f2efebb9876a51a233f658df4b9215499876ba04
Bartłomiej Łagosz <lagosz.bartlomiej@gmail.com>
2020-03-31 16:30:27 +0100

    Revert "Done 2"
    This reverts commit 75f28109

--- Lab_2/example1.m
+++ Lab_2/example1.m
@@ -1,21 +1,5 @@
-# assignment operators
-# binary operators
-# transposition
+A = zeros(5); # create 5x5 matrix filled with zeros
+D = A.+B' ;   # add element-wise A with transpose of B
 
-C = -A;     # assignemnt with unary expression
-C = B' ;    # assignemnt with matrix transpose
-C = A+B ;   # assignemnt with binary addition
-C = A-B ;   # assignemnt with binary substraction
-C = A*B ;   # assignemnt with binary multiplication
-C = A/B ;   # assignemnt with binary division
-C = A.+B ;  # add element-wise A to B
-C = A.-B ;  # substract B from A 
-C = A.*B ;  # multiply element-wise A with B
-C = A./B ;  # divide element-wise A by B
-
-C += B ;  # add B to C 
-C -= B ;  # substract B from C 
-C *= A ;  # multiply A with C
-C /= A ;  # divide A by C
-
-
+for j = 1:10 
+    print j;
