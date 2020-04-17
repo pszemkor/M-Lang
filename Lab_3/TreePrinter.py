4f71a3069863da82d220c9923759b0fe41ceeeb3
okarinbro <prz.jab98@gmail.com>
2020-04-17 14:42:29 +0200

    Add printTree method for every component

--- Lab_3/TreePrinter.py
+++ Lab_3/TreePrinter.py
@@ -21,7 +21,98 @@ class TreePrinter:
     def printTree(self, indent=0):
         pass
 
-    @addToClass(Error)
+    @addToClass(FloatNum)
     def printTree(self, indent=0):
         pass
 
+    @addToClass(Tuple)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Variable)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(BinExpr)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Program)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(InstructionsOpt)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Instructions)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Instruction)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Printable)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Loop)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(ArrayRange)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(IfStatement)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Assign)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(ArrayPart)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(String)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Eye)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Ones)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Zeros)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(UnaryExpression)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Matrix)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Rows)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(Row)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(LogicalOperator)
+    def printTree(self, indent=0):
+        pass
+
+    @addToClass(ElseStatement)
+    def printTree(self, indent=0):
+        pass
